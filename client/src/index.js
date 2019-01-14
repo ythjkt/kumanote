e44a22d85e6d58d0befdedf6a22516fb3dba604f
bebaty01 <ythjkt@gmail.com>
2019-01-13 19:29:47 -0800

    Adds auto login for users with jwtToken in local storage

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,11 @@ import { Provider } from 'react-redux'
 import { createStore, compose, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 import thunk from 'redux-thunk'
+
+import jwt_decode from 'jwt-decode'
+import setAuthToken from './utils/setAuthToken'
+import { setCurrentUser } from './actions/user'
+
 import App from './components/App'
 
 const initialState = {}
@@ -18,6 +23,13 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 )
+
+// Check if there is user info in local storage
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken)
+  const decoded = jwt_decode(localStorage.jwtToken)
+  store.dispatch(setCurrentUser(decoded))
+}
 
 render(
   <Provider store={store}>
