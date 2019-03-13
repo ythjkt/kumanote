dd1f44cb3a29603e62197817d073cd1eb1a1c515
bebaty01 <ythjkt@gmail.com>
2019-03-13 18:34:53 +0900

    Adds authentication for mongodb

--- api/config/keys.js
+++ api/config/keys.js
@@ -1,6 +1,8 @@
 module.exports = {
-  mongoURI: process.env.MONGO_HOST,
+  mongoHost: process.env.MONGO_HOST,
   mongoDB: process.env.MONGO_DB,
   mongoPort: process.env.MONGO_PORT,
+  mongoUser: process.env.MONGO_INITDB_ROOT_USERNAME,
+  mongoPassword: process.env.MONGO_INITDB_ROOT_PASSWORD,
   secretOrKey: 'secret'
 }
