36fc99c9dadc870b61d806145f3e883bd8864457
ythjkt <ythjkt@gmail.com>
2019-03-20 18:46:16 +0900

    Fixes mongodb connection error

--- api/config/keys.js
+++ api/config/keys.js
@@ -2,7 +2,7 @@ module.exports = {
   mongoHost: process.env.MONGO_HOST,
   mongoDB: process.env.MONGO_DB,
   mongoPort: process.env.MONGO_PORT,
-  mongoUser: process.env.MONGO_USERNAME,
-  mongoPassword: process.env.MONOGO_PASSWORD,
+  mongoUser: process.env.MONGO_INITDB_ROOT_USERNAME,
+  mongoPassword: process.env.MONGO_INITDB_ROOT_PASSWORD,
   secretOrKey: 'secret'
 }
