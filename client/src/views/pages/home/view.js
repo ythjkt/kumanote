1d1c2c7e12ae5d4cc7a888b03bf525ff24dffb63
ythjkt <ythjkt@gmail.com>
2019-04-28 11:22:52 +0900

    Fixes visual bug on smaller phones

--- client/src/views/pages/home/view.js
+++ client/src/views/pages/home/view.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import theme from '../../../const/theme'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
@@ -6,15 +6,16 @@ import { Link } from 'react-router-dom'
 import { Button } from '../../../components/button/'
 import Logo from '../../../components/logo/'
 
-import { FlexCol, FlexRow, Section, ImageFrame, SectionTitle } from './style'
+import { Section, ImageFrame, SectionTitle } from './style'
+import { BREAK_POINT } from '../../../components/layout'
 
 export const Overview = () => {
   const ThisSection = styled(Section)`
     padding: 180px 0 120px;
-    height: 98vh;
+    min-height: 60vh;
 
-    @media (max-width: 1000px) {
-      padding: 180px 20px 120px;
+    @media (max-width: ${BREAK_POINT}px) {
+      padding: 120px 20px 120px;
     }
   `
   const ThisTitle = styled.h2`
@@ -33,7 +34,7 @@ export const Overview = () => {
   const HeroImage = styled(ImageFrame)`
     grid-area: image;
 
-    @media (max-width: 1000px) {
+    @media (max-width: ${BREAK_POINT}px) {
       display: none;
     }
   `
@@ -44,7 +45,7 @@ export const Overview = () => {
     grid-template-columns: auto auto;
     grid-gap: 20px;
 
-    @media (max-width: 1000px) {
+    @media (max-width: ${BREAK_POINT}px) {
       grid-template-columns: auto;
       grid-template-rows: auto auto;
       justify-items: start;
@@ -62,7 +63,7 @@ export const Overview = () => {
       'button image';
     justify-items: start;
 
-    @media (max-width: 1000px) {
+    @media (max-width: ${BREAK_POINT}px) {
       grid-template-columns: auto;
       grid-template-rows: auto auto auto;
       grid-template-areas:
@@ -91,7 +92,7 @@ export const Overview = () => {
         </Buttons>
 
         <HeroImage width="600">
-          <img src="/img/screenshot.1.png" />
+          <img src="/img/screenshot.1.png" alt="screenshot" />
         </HeroImage>
       </Grid>
     </ThisSection>
@@ -119,7 +120,7 @@ export const Features = () => {
     flex-direction: column;
     padding: 80px 0;
 
-    @media (max-width: 1000px) {
+    @media (max-width: ${BREAK_POINT}px) {
       padding: 80px 20px;
     }
   `
@@ -136,9 +137,9 @@ export const Features = () => {
     grid-template-areas: 'image copy';
     align-items: start;
     grid-gap: 40px;
-    max-width: 1000px;
+    max-width: ${BREAK_POINT}px;
 
-    @media (max-width: 1000px) {
+    @media (max-width: ${BREAK_POINT}px) {
       grid-template-columns: auto;
       grid-template-rows: auto auto;
       grid-template-areas: 'image' 'copy';
